6fcae9688522ba967c02e97e21ede70f3f9b9bad
aj <ali.becerra1224@gmail.com>
2021-12-07 22:24:38 -0800

    i finally added day1-2 to this problem now, also my first fully complete aoc problem

--- aoc2021/day1.py
+++ aoc2021/day1.py
@@ -10,3 +10,24 @@ for i, element in enumerate(mylist):
         M_Increase = M_Increase + 1
 
 print(M_Increase)
+
+sumlist = []
+testlist = [199, 200, 208, 210, 200, 207, 240, 269, 260,263]
+
+
+for j, element in enumerate(mylist):
+    if j == 0:
+        print("nope")
+    else:
+        sumlist.append(sum(mylist[j-1:j+2:1]))
+print(sumlist)
+
+sum_increase = 0
+
+for k, element in enumerate(sumlist):
+    if k == 0:
+        print("first data input lmao")
+    elif sumlist[k-1] < sumlist[k]:
+        sum_increase = sum_increase + 1
+
+print(sum_increase)
