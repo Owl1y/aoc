37665af2486553ac7af1ea84f4ca172c9a2a75ad
aj <ali.becerra1224@gmail.com>
2021-12-07 20:45:22 -0800

    finished day1-1 2021 in py and added a text file for the data

--- aoc2021/day1.py
+++ aoc2021/day1.py
@@ -1,1 +1,12 @@
-# yoo this is a comment lel
+M_Increase = 0
+
+with open('day1.txt','r') as f:
+    mylist = [int(x) for x in f]
+
+for i, element in enumerate(mylist):
+    if i == 0:
+        print("first data input lmao")
+    elif mylist[i-1] < mylist[i]:
+        M_Increase = M_Increase + 1
+
+print(M_Increase)
