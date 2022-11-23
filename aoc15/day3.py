4cb0dde92a436b81035303d7645d767d53db9711
aj <ali.becerra1224@gmail.com>
2022-11-22 19:58:51 -0800

    did part one of day 3

--- aoc15/day3.py
+++ aoc15/day3.py
@@ -4,36 +4,41 @@ with open("day3.txt", "r") as client_nums:
     lines = client_nums.readlines()
     lines = [line.rstrip() for line in lines]
 
+input = lines[0]
 
-coords_of_houses = []
-# x_axis = 0
-# y_axis = 0
+coords_of_houses = [[0,0]]
 count_x = 0
 count_y = 0
 
-for i in lines[0]:
+for i in input:
     if i == '>':
         count_x = count_x + 1
+        coords_of_houses.append([count_x, count_y])
     if i == '<':
         count_x = count_x - 1
+        coords_of_houses.append([count_x, count_y])
     if i == '^':
         count_y = count_y + 1
+        coords_of_houses.append([count_x, count_y])
     if i == 'v':
         count_y = count_y - 1
+        coords_of_houses.append([count_x, count_y])
         
+# got info to sort list of list from here https://stackoverflow.com/questions/12198468/how-to-remove-duplicate-lists-in-a-list-of-list
+sorted_houses = set(map(tuple,coords_of_houses))
+sorted_houses_list = map(list,sorted_houses)
+
+print(len(sorted_houses))
+
+# Part 2
+
+santa_list = []
+for n in enumerate(input):
+    if n%2 == 0:
+        santa_list.append(n)
+
+print(santa_list)
 
 
-sum_east = lines[0].count('>')
-sum_west = lines[0].count('<')
-sum_north = lines[0].count('^')
-sum_south = lines[0].count('v')
-
-# print(f"North is {sum_north}\nEast is {sum_east}\nSouth is {sum_south}\nWest is {sum_west}")
-
-print((sum_north * sum_east) - (sum_south * sum_west))
-print( count_y, count_x)
-
-
-
-
-
+Santa_list = [n for n in range(len(lines[0])) if n%2 == 0]
+print(Santa_list)
