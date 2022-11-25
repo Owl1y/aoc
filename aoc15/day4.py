4c2347beb0344a2a52e451ffc04176268e06d661
aj <ali.becerra1224@gmail.com>
2022-11-24 17:28:16 -0800

    did day 4 part 1 and 2, was much easier than day 3

---
+++ aoc15/day4.py
@@ -0,0 +1,28 @@
+import hashlib
+
+input = "ckczppom"
+five0 = "00000"
+six0 = "000000"
+
+x = 0
+input_num = bytes(input + str(x), 'utf-8')
+result = hashlib.md5(input_num)
+
+while str(result.hexdigest())[0:5] != five0:
+    x = x + 1
+    input_num = bytes(input + str(x), 'utf-8')
+    result = hashlib.md5(input_num)
+
+print(result.hexdigest())
+print(x)
+
+# Part 2
+while str(result.hexdigest())[0:6] != six0:
+    x = x + 1
+    input_num = bytes(input + str(x), 'utf-8')
+    result = hashlib.md5(input_num)
+    # print(result.hexdigest())
+print(result.hexdigest())
+print(x)
+
+
