59f32fb2545a76867b864d2a53004acda4f016ca
aj <ali.becerra1224@gmail.com>
2022-11-27 21:35:55 -0800

    finished part 1 of day 5

--- aoc15/day5.py
+++ aoc15/day5.py
@@ -5,7 +5,6 @@ with open("day5.txt", "r") as client_nums:
     lines = [line.rstrip() for line in lines]
 
 nice_words = 0
-
 for i in range(len(lines)):
     # these two used for regex extraction 
     # https://www.tutorialspoint.com/extract-only-characters-from-given-string-in-python
@@ -14,7 +13,6 @@ for i in range(len(lines)):
     # for double letters, https://stackoverflow.com/questions/1023902/it-is-possible-to-match-a-character-repetition-with-regex-how 
     double_let = "".join(re.findall(r"(.)\1+", lines[i]))
     
-    
     forb_ab = lines[i].find("ab")
     forb_cd = lines[i].find("cd")
     forb_pq = lines[i].find("pq")
@@ -22,12 +20,8 @@ for i in range(len(lines)):
     forb_let = [forb_ab, forb_cd, forb_pq, forb_xy] 
     forb_let_cut = [*set(forb_let)] 
 
-    print(f"{lines[i]} {cut_v} {double_let} {len(double_let)} {forb_let_cut} {len(forb_let_cut)}")
-    
-    if (len(forb_let_cut) > 1) and (len(cut_v) >= 3) and (len(double_let) >= 1):
-        # print(f"{lines[i]}   {cut_v}    {double_let}   {len(double_let)}    {forb_let_cut}    {len(forb_let_cut)}")
+    if (len(cut_v) >= 3) and (len(double_let) >= 1) and (len(forb_let_cut) == 1):
+        print(f"{lines[i]}   {cut_v}    {double_let}   {len(double_let)}    {forb_let_cut}    {len(forb_let_cut)}")
+        nice_words = nice_words + 1 
 
-        nice_words = nice_words + 1
-
-# print(cut_v, double_let, lines[i], forbidden_let)
 print(nice_words)
